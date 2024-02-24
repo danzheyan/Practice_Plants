b7342c5f203c5c7bbfdc9cc28faa6c9c2cea884f
Aditya Ramesh <adityaramesh15@gmail.com>
2024-02-24 15:38:36 -0600

    Completed outlier removal

--- Model/clean.py
+++ Model/clean.py
@@ -1,13 +1,26 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-def clean(data, columns_to_keep=[]):
 
+def clean(data: pd.DataFrame, columns_to_keep=[]) -> pd.DataFrame:
+    # Removing Duplicates and Null Values
     data.dropna(inplace=True)
     data.drop_duplicates(inplace=True)
 
-    new_data = data[columns_to_keep]
+    # Removing Outliers using IQR
+    numeric_data = data.select_dtypes(include="number")
+
+    Q1 = numeric_data.quantile(0.25)
+    Q3 = numeric_data.quantile(0.75)
+    IQR = Q3 - Q1
+
+    clean_data = numeric_data[~((numeric_data < (Q1 - 1.5 * IQR)) | (numeric_data > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+    # Selecting Columns of interest
+    new_data = clean_data[columns_to_keep]
 
     return new_data
 
-def scale():
+
+def scale(data):
     ...
--- Model/main.py
+++ Model/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import clean
 
 raw_data = pd.read_csv("raw-dataset.csv")
-features = ['temperature', 'humidity', 'ph', 'label']
+features = ["temperature", "humidity", "ph"]
 
-cleaned_data = clean.clean(raw_data,features)
-
+cleaned_data = clean.clean(raw_data, features)
+print(cleaned_data)
